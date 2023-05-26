8cda8ca250e6e5ef218f0c49f64ce15bba06979c
k <vellutialessandro03@gmail.com>
2023-05-26 16:26:57 +0200

    previsione-separata-finale

--- allerta.js
+++ allerta.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const {insertAllerta,listAllerte,getAllerta,updateAllerta,deleteAllerta} = require("./allerta.dao")
 
-const router = Router;
+const router = Router();
 
 router.put('/allerta/:', async (req,res) =>{
     const allerta= await updateAllerta(req.body.descrizione,req.body.data_inizio,req.body.data_fine)
---
+++ previsione.js
@@ -0,0 +1,62 @@
+const { Router } = require("express");
+const bodyParser = require("body-parser");
+const { insertPrevisione, getPrevisione, updatePrevisione, listPrevisioni, deletePrevisione } = require("./previsione.dao");
+
+const router = Router();
+router.use(bodyParser.json())
+//inserisci previsione
+router.post('/', async (req, res) => {
+    const newpre= await insertPrevisione(req.body)
+    res.json(newpre)
+})
+
+//get previsione
+router.get('/:idPrevisione', async (req, res) => {
+    const prev = await getPrevisione({ id: req.params.idPrevisione });
+    res.json(prev)
+});
+
+//elimina previsione
+router.delete('/:idPrevisione', async (req, res) => {
+    const prev = await deletePrevisione({ id: req.params.idPrevisione });
+    const risposta={
+        message: "Previsione eliminata"
+    }
+    res.json(risposta)
+});
+
+//modifica revisione
+router.put('/:idPrevisione', async (req, res) => {
+    const aggiornati = await updatePrevisione(req.body, { id: req.params.idPrevisione });
+    const risposta={
+        message: "Previsione aggiornata"
+    }
+    res.json(risposta)
+});
+
+
+router.get('/giornaliera/:prevData', async (req, res) => {
+    const lista =await listPrevisioni({data: req.params.prevData})
+    res.json(lista)
+});
+
+
+router.get('/futura/:prevData', async (req, res) => {
+    
+    const listafutura=[]
+    const datasplit= req.params.prevData.split("-")
+
+    for(let i=1;i<8;i++){
+        let nuovadata= [].concat(datasplit)
+        const giorno= parseInt(nuovadata[2])+i
+        nuovadata[2]=giorno
+        nuovadata=nuovadata.join('-')
+        const lista =await listPrevisioni({data: nuovadata})        
+        listafutura.push(lista)
+    }
+    
+    res.json(listafutura)
+});
+
+
+module.exports= router
